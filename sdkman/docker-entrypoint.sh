58138514ff03c64ab1500b7781499f9b569772a7
RodrigoDornelles <rodrigo@dornelles.me>
2023-10-25 17:16:37 -0300

    fix: docker-entrypoint.sh in sdkman

--- sdkman/docker-entrypoint.sh
+++ sdkman/docker-entrypoint.sh
@@ -6,8 +6,11 @@ mkdir -p /tmp/android /tmp/sdkman-tmp /tmp/sdkman-candidates
 if [[ ("$1" = 'sh' || "$1" = 'bash') && "$2" = "-c" ]]; then
     shift 2
     bash -c "source /root/.bashrc && source $SDKMAN_DIR/bin/sdkman-init.sh && ($@)"
+elif [[ ("$1" = 'sh' || "$1" = 'bash') && $# -eq 2 ]]; then
+    r=$(<"$2")
+    bash -c "source /root/.bashrc && source $SDKMAN_DIR/bin/sdkman-init.sh && ($r)"
 elif [ $# -gt 0 ]; then
-    bash -c "source /root/.bashrc && source $SDKMAN_DIR/bin/sdkman-init.sh && ($@)"
+    bash -c "source /root/.bashrc && source $SDKMAN_DIR/bin/sdkman-init.sh && ($*)"
 else
     source /root/.bashrc
     source $SDKMAN_DIR/bin/sdkman-init.sh
